4fc463296e1a55c20287f37349ddc9c0d89e3d7f
Herun Tarun <herun.s@gmail.com>
2026-07-14 14:42:27 -0400

    added book_viewer, and updated table of contents

---
+++ lesson-2/book_viewer_starter/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    with open('book_viewer/data/toc.txt', 'r') as file:
+        contents = file.readlines()
+    return render_template('index.html', contents=contents)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5003)
