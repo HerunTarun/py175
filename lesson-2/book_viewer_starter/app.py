2d3036483c48799c8b92979f2986bf9bfc926235
Herun Tarun <herun.s@gmail.com>
2026-07-14 15:59:12 -0400

    updated book_viewer_to use route parameters

--- lesson-2/book_viewer_starter/app.py
+++ lesson-2/book_viewer_starter/app.py
@@ -8,19 +8,24 @@ def index():
         contents = file.readlines()
     return render_template('home.html', contents=contents)
 
-@app.route("/chapters/1")
-def chapter1():
-    chapter_title = "Chapter 1"
-
-    with open('book_viewer/data/toc.txt', 'r') as file:
+@app.route("/chapters/<page_num>")
+def chapter(page_num):
+    with open("book_viewer/data/toc.txt", "r") as file:
         contents = file.readlines()
 
-    with open('book_viewer/data/chp1.txt', 'r') as file:
-        chapter = file.read()
+    chapter_name = contents[int(page_num) - 1]
+    chapter_title = f"Chapter {page_num}: {chapter_name}"
+
+    with open(f"book_viewer/data/chp{page_num}.txt") as file:
+        chapter_content = file.read()
 
     return render_template('chapter.html',
-                           chapter_title=chapter_title,
-                           chapter=chapter,
-                           contents=contents)
+                            chapter_title=chapter_title,
+                            contents=contents,
+                            chapter=chapter_content)
+@app.route("/show/<name>")
+def show_name(name):
+    return name
+
 if __name__ == "__main__":
     app.run(debug=True, port=5003)
