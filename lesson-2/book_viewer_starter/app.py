41a0d0f50f3d0885c476f5960d039e6c2774b48c
Herun Tarun <herun.s@gmail.com>
2026-07-27 14:09:25 -0400

    added a 404 page

--- lesson-2/book_viewer_starter/app.py
+++ lesson-2/book_viewer_starter/app.py
@@ -15,6 +15,10 @@ def in_paragraphs(text):
                             if paragraph]
     return ''.join(formatted_paragraphs)
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
 @app.route("/")
 def index():
     return render_template('home.html', contents=g.contents)
