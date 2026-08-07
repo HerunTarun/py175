cad2004113b3e393c9a6d8dd79cec7ac88224427
Herun Tarun <herun.s@gmail.com>
2026-08-07 14:12:29 -0400

    added the ability to create new todo lists

--- lesson4/todo_starter/app.py
+++ lesson4/todo_starter/app.py
@@ -1,14 +1,45 @@
-from flask import Flask, render_template
+from uuid import uuid4
+from flask import (Flask,
+                   redirect, render_template,
+                   request,
+                   session,
+                   url_for,
+)
 
 app = Flask(__name__)
+app.secret_key='secret1'
+
+@app.before_request
+def initialize_session():
+    if 'lists' not in session:
+        session['lists'] = []
 
 @app.route("/")
 def index():
+    return redirect(url_for('get_lists'))
+
+@app.route('/lists/new')
+def add_todo_list():
+    return render_template('new_list.html')
+
+@app.route('/lists')
+def get_lists():
     lists = [
         {"title": "Lunch Groceries", "todos": []},
         {"title": "Dinner Groceries", "todos": []},
     ]
-    return render_template("lists.html", lists=lists)
+    return render_template("lists.html", lists=session['lists'])
+
+@app.route('/lists', methods=["POST"])
+def create_list():
+    title = request.form["list_title"].strip()
+    session['lists'].append({
+        'id': str(uuid4()),
+        'title': title,
+        'todos': [],
+        })
+    session.modified = True
+    return redirect(url_for('get_lists'))
 
 if __name__ == "__main__":
     app.run(debug=True, port=5003)
