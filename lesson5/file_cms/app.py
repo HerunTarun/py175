5b584f0a18bbbdf9c595e115a4169e70f5e84251
Herun Tarun <herun.s@gmail.com>
2026-08-12 13:01:17 -0400

    added functionality to view text files using send_from_directory

--- lesson5/file_cms/app.py
+++ lesson5/file_cms/app.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template
+from flask import (
+    Flask,
+    render_template,
+    send_from_directory,
+    )
 import os
 
 app = Flask(__name__)
@@ -6,10 +10,17 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     root = os.path.abspath(os.path.dirname(__file__))
-    data_dir = os.path.join(root, "data")
+    data_dir = os.path.join(root, "src", "file_cms", "data",)
     files = [os.path.basename(path) for path in os.listdir(data_dir)]
+
     return render_template('index.html', files=files)
 
+@app.route("/<file_name>")
+def document(file_name):
+    root = os.path.abspath(os.path.dirname(__file__))
+    data_dir = os.path.join(root, "src", "file_cms", "data")
+
+    return send_from_directory(data_dir, file_name)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5003)
