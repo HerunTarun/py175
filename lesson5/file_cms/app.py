73e02f2e5cd50fdb7ade0e82b3748433ec3752ad
Herun Tarun <herun.s@gmail.com>
2026-08-13 13:30:21 -0400

    added a new document creation page, and added routes to handle form submission

--- lesson5/file_cms/app.py
+++ lesson5/file_cms/app.py
@@ -63,12 +63,39 @@ def update_document(file_name):
     data_dir = get_data_path()
     file_path = os.path.join(data_dir, file_name)
 
-    new_contents = request.form['contents']
+    new_contents = request.form.get('contents', '')
     with open(file_path, 'w') as f:
         f.write(new_contents)
 
     flash(f"{file_name} has been updated.", "success")
     return redirect(url_for('index'))
+
+@app.route("/new")
+def new_document():
+    title = 'New Document'
+    return render_template('new_document.html', title=title)
+
+@app.route("/new", methods=["post"])
+def create_document():
+    document_name = request.form.get('document_name', '').strip()
+
+    data_dir = get_data_path()
+    new_file_path = os.path.join(data_dir, document_name)
+
+    if len(document_name) == 0:
+        flash("A name is required.", "error")
+        title = 'New Document'
+        return render_template('new_document.html', title=title), 422
+    elif os.path.exists(new_file_path):
+        flash(f"{document_name} already exists.", "error")
+        title = 'New Document'
+        return render_template('new_document.html', title=title), 422
+    else:
+        with open(new_file_path, "w") as f:
+            f.write("")
+
+        flash(f"{document_name} has been created.", "success")
+        return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run(debug=True, port=5003)
