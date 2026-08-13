a8f501cc306ee43134fc0b1472ab0e21fb5ec343
Herun Tarun <herun.s@gmail.com>
2026-08-13 14:37:44 -0400

    added button to delete documents

--- lesson5/file_cms/app.py
+++ lesson5/file_cms/app.py
@@ -20,7 +20,6 @@ def get_data_path():
 
 @app.route("/")
 def index():
-    root = os.path.abspath(os.path.dirname(__file__))
     data_dir = get_data_path()
     files = [os.path.basename(path) for path in os.listdir(data_dir)]
 
@@ -41,7 +40,7 @@ def document(file_name):
             return send_from_directory(data_dir, file_name)
     else:
         flash(f"{file_name} does not exist.", "error")
-        return redirect(url_for('index', file_name = file_name))
+        return redirect(url_for('index', file_name=file_name))
 
 @app.route("/<file_name>/edit")
 def edit_document(file_name):
@@ -56,9 +55,9 @@ def edit_document(file_name):
                            contents=contents)
     else:
         flash(f"{file_name} does not exist.", "error")
-        return redirect(url_for('index', file_name = file_name))
+        return redirect(url_for('index', file_name=file_name))
 
-@app.route("/<file_name>", methods=["post"])
+@app.route("/<file_name>", methods=["POST"])
 def update_document(file_name):
     data_dir = get_data_path()
     file_path = os.path.join(data_dir, file_name)
@@ -75,7 +74,7 @@ def new_document():
     title = 'New Document'
     return render_template('new_document.html', title=title)
 
-@app.route("/new", methods=["post"])
+@app.route("/new", methods=["POST"])
 def create_document():
     document_name = request.form.get('document_name', '').strip()
 
@@ -96,6 +95,19 @@ def create_document():
 
         flash(f"{document_name} has been created.", "success")
         return redirect(url_for('index'))
+
+@app.route("/<file_name>/delete", methods=["POST"])
+def delete_document(file_name):
+    data_dir = get_data_path()
+    file_path = os.path.join(data_dir, file_name)
+
+    if os.path.isfile(file_path):
+        os.remove(file_path)
+        flash(f"{file_name} has been deleted.", "success")
+    else:
+        flash(f"{file_name} does not exist.", "error")
+
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run(debug=True, port=5003)
